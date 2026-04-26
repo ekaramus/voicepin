61c884a09eded884e33341a39d27caa521b28e13
Ewa Ramus <you@gmail.com>
2026-04-26 11:17:11 +0200

    test: configure path alias for vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vitest/config'
+import path from "node:path";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   test: {
-    environment: 'jsdom',
+    environment: "jsdom",
     globals: true,
     setupFiles: ["./src/test/setup.ts"],
   },
-})
+});
