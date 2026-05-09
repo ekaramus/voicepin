4be2da22c3610b800f055dfcbec886141f70f075
Ewa Ramus <you@gmail.com>
2026-05-09 17:52:21 +0200

    chore: add app metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "VoicePin",
   description: "Tiny voice snapshots",
   icons: {
